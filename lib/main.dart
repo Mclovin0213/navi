ad7768650cf62c0c52631f74332f07b000a7be91
Mclovin0213 <javellaneda0213@gmail.com>
2023-05-02 11:26:58 -0700

    Add form for AddPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timeline_tile/timeline_tile.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = Placeholder();
+        page = AddPage();
         break;
       case 1:
         page = TimelinePage();
@@ -83,14 +83,192 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class TimelinePage extends StatelessWidget {
+class AddPage extends StatefulWidget {
+  const AddPage({Key? key}) : super(key: key);
+
+  static const List<String> types = <String>['Project', 'Event', 'Task'];
+  static const List<TimeOfDay> times = <TimeOfDay>[
+    TimeOfDay(hour: 8, minute: 00),
+    TimeOfDay(hour: 9, minute: 00),
+    TimeOfDay(hour: 10, minute: 00),
+    TimeOfDay(hour: 11, minute: 00),
+    TimeOfDay(hour: 12, minute: 00),
+    TimeOfDay(hour: 13, minute: 00),
+    TimeOfDay(hour: 14, minute: 00),
+    TimeOfDay(hour: 15, minute: 00),
+    TimeOfDay(hour: 16, minute: 00),
+    TimeOfDay(hour: 17, minute: 00),
+    TimeOfDay(hour: 18, minute: 00),
+    TimeOfDay(hour: 19, minute: 00),
+    TimeOfDay(hour: 20, minute: 00),
+    TimeOfDay(hour: 21, minute: 00),
+    TimeOfDay(hour: 22, minute: 00),
+    TimeOfDay(hour: 23, minute: 00),
+    TimeOfDay(hour: 0, minute: 00),
+  ];
+
+  static const List<Color> colors = <Color>[
+    Colors.deepPurple,
+    Colors.green,
+    Colors.red,
+    Colors.blue,
+    Colors.yellow,
+    Colors.orange,
+    Colors.pink
+  ];
+
+
+  @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return TimelineTile(
-      axis: TimelineAxis.horizontal,
-      alignment: TimelineAlign.center,
-      isFirst: true,
-    ); //TimelineTile
+    String typeValue = AddPage.types.first;
+    TimeOfDay startTimeValue = AddPage.times.first;
+    TimeOfDay endTimeValue = AddPage.times[1];
+    Color color = AddPage.colors.first;
+
+    return SafeArea(
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownButtonFormField(
+                value: typeValue,
+                items: AddPage.types.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    typeValue = value!;
+                  });
+                }
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Name of path'
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: 'Short Description (optional)'
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              DropdownButtonFormField(
+                icon: Icon(Icons.access_time),
+                value: startTimeValue,
+                items: AddPage.times.map<DropdownMenuItem<TimeOfDay>>((TimeOfDay value) {
+                  return DropdownMenuItem<TimeOfDay>(
+                    value: value,
+                    child: Text(value.format(context)),
+                  );
+                }).toList(),
+                onChanged: (TimeOfDay? value) {
+                  setState(() {
+                    startTimeValue = value!;
+                  });
+                }
+              ),
+              DropdownButtonFormField(
+                icon: Icon(Icons.access_time),
+                value: endTimeValue,
+                items: AddPage.times.map<DropdownMenuItem<TimeOfDay>>((TimeOfDay value) {
+                  return DropdownMenuItem<TimeOfDay>(
+                    value: value,
+                    child: Text(value.format(context)),
+                  );
+                }).toList(),
+                onChanged: (TimeOfDay? value) {
+                  setState(() {
+                    endTimeValue = value!;
+                  });
+                }
+              ),
+              DropdownButtonFormField(
+                  icon: Icon(Icons.color_lens_outlined),
+                  value: color,
+                  items: AddPage.colors.map<DropdownMenuItem<Color>>((Color value) {
+                    return DropdownMenuItem<Color>(
+                      value: value,
+                      child: Text(value.toString())
+                    );
+                  }).toList(),
+                  onChanged: (Color? value) {
+                    setState(() {
+                      color = value!;
+                    });
+                  }
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+
+                  }
+                },
+                child: Text('Submit')
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
+}
+
+
+class TimelinePage extends StatelessWidget {
+  const TimelinePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        TimelineTile(
+          axis: TimelineAxis.horizontal,
+          alignment: TimelineAlign.center,
+          isFirst: true,
+        ),
+      ]
+    );
+  }
+}
+
+class Path {
+  final String pathType;
+  final String pathName;
+  final String pathDesc;
+  final TimeOfDay pathStart;
+  final TimeOfDay pathEnd;
+
+  Path({
+    required this.pathType,
+    required this.pathName,
+    required this.pathDesc,
+    required this.pathStart,
+    required this.pathEnd,
+  });
 }
 
