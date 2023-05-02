d135837aa8375ab2eaebc2d1820b9edcbce98a52
Mclovin0213 <javellaneda0213@gmail.com>
2023-05-01 19:07:02 -0700

    Init Commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timeline_tile/timeline_tile.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Navi',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selectedIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = Placeholder();
+        break;
+      case 1:
+        page = TimelinePage();
+        break;
+      case 2:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('No widget for $selectedIndex');
+    }
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add Path'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timeline_outlined),
+            label: 'Timeline'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined),
+            label: 'Edit Paths'
+          )
+        ],
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+      ),
+      body: page,
+    );
+  }
+}
+
+class TimelinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TimelineTile(
+      alignment: TimelineAlign.center,
+      endChild: Container(
+        constraints: const BoxConstraints(
+          minHeight: 120,
+        ),
+        color: Colors.lightGreenAccent,
+      ),
+      startChild: Container(
+        color: Colors.amberAccent,
+      ),
+    );
+  }
+}
+
